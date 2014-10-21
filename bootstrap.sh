01e1b5f658eb3cd5169c5a6377e89bbaef298e20
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-10-21 02:34:46 +0200

    full configure and build commands from Rocker and wch/r-source

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,8 +58,37 @@ recompile_r() {
   git pull
 
   sudo yum install -y gcc-gfortran.x86_64
+  export PATH=$CACHE_BASE_PATH/texlive/bin/x86_64-linux:$PATH
+
   tools/rsync-recommended
-  ./configure
+  R_PAPERSIZE=letter \
+    R_BATCHSAVE="--no-save --no-restore" \
+    R_BROWSER=xdg-open \
+    PAGER=/usr/bin/pager \
+    PERL=/usr/bin/perl \
+    R_UNZIPCMD=/usr/bin/unzip \
+    R_ZIPCMD=/usr/bin/zip \
+    R_PRINTCMD=/usr/bin/lpr \
+    LIBnn=lib \
+    AWK=/usr/bin/awk \
+    CFLAGS="-pipe -std=gnu99 -Wall -pedantic -O3" \
+    CXXFLAGS="-pipe -Wall -pedantic -O3" \
+    ./configure --enable-R-shlib \
+                --without-blas \
+                --without-lapack \
+                --without-readline
+
+  (cd doc/manual && make front-matter html-non-svn)
+
+  echo -n 'Revision: ' > SVN-REVISION
+  git log --format=%B -n 1 \
+    | grep "^git-svn-id"    \
+    | sed -E 's/^git-svn-id: https:\/\/svn.r-project.org\/R\/[^@]*@([0-9]+).*$/\1/' \
+    >> SVN-REVISION
+  echo -n 'Last Changed Date: ' >>  SVN-REVISION
+  git log -1 --pretty=format:"%ad" --date=iso | cut -d' ' -f1 >> SVN-REVISION
+
+  make
 }
 
 cd $CACHE_BASE_PATH
